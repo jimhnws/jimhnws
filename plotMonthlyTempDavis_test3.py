01a52f6ede680f69dd97555bbc65494ba79d12d6
jimhnws <64051735+jimhnws@users.noreply.github.com>
2023-07-31 00:21:24 -0400

    Add files via upload

--- plotMonthlyTempDavis_test3.py
+++ plotMonthlyTempDavis_test3.py
@@ -8,32 +8,25 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib.colors as mcolors
 import numpy as np
-import numpy as np
+import seaborn as sns
 from scipy.interpolate import make_interp_spline
 import calcTimeNow
-import month_Days
+import getDaysInMonth
 import getNameNumbers
 
 
 # In[ ]:
 
 
-#
-# Make some graphs - temperature line plots first
-# For just one month
-#
-
-# Determine the date
-
-sta = ['Davis', 'Tempest']
+sta = ['Davis']
 
 for qwe in sta:
+
+    wl = getNameNumbers.davis()
+    xls_filename, xls_fullfile, path_name, date, month_name, year, r = wl[0], wl[1], \
+    wl[2], wl[3], wl[4], wl[5], wl[6]       
     
-    wl = getNameNumbers.tempest_ec2()
-    xls_filename, xls_fullfile, path_name, date, this_month, thisYear, r = wl[0], wl[1], \
-    wl[2], wl[3], wl[4], wl[5], wl[6]   
-    
-    wxdata = f'{path_name}{this_month}_{thisYear}_{qwe}.xlsx'
+    wxdata = f'{path_name}{month_name}_{year}_{qwe}.xlsx'
     df = pd.read_excel(wxdata, skiprows=[0,1])
     if qwe == 'Davis':
         df = df.drop(df.columns[[3,4,5,6,7,8,9,10,11,12,13,14]], axis=1)
@@ -44,17 +37,15 @@ for qwe in sta:
        
     df = df.drop(df.index[date:r])
     df['Date'] = df['Date'].astype(int)
-    
+        
     HI = df['High']
     LO = df["Low"]
     DATE = df["Date"]
-    print(DATE)
 
     y = HI.to_numpy()
     y1 = LO.to_numpy()
     x = DATE.to_numpy()
-
-    
+        
     #define x as 200 equally spaced values between the min and max of original x 
     #xnew = np.linspace(x.min(), x.max(), 200) 
 
@@ -64,21 +55,27 @@ for qwe in sta:
     #LOspl = make_interp_spline(x, y1, k=2)
     #y1_smooth = LOspl(xnew)
     
-    path1 = '/var/www/html/000/'
+    path1 = '/var/www/html/trclimate/'
+    
+    #sns.set_style("whitegrid")
+    #sns.lineplot(data = df[['High', 'Low']])
+    #sns.lineplot(data=df[['A', 'B', 'C', 'D']])
+          
     plt.figure(figsize= (10,6))
     plt.locator_params(axis='x', nbins = date)
-    plt.xlim(1,r)
-    plt.ylim(0, 105)
+    plt.xlim(1,date)
     plt.xticks(fontsize=12)
     plt.xlabel('Date', fontsize=12, fontweight ='bold')
+            
+    plt.ylim(0, 105)
     plt.yticks(fontsize=12)
     plt.ylabel('Temperature (F)', fontsize=12, fontweight ='bold')
     plt.locator_params(axis='y', nbins=20)
-    plt.title(f'{this_month} {thisYear} Temperatures - {qwe}', fontsize=12, fontweight ='bold')
+    plt.title(f'{month_name} {year} Temperatures', fontsize=12, fontweight ='bold')
+    #plt.grid(True)
     plt.grid(axis = "y", linewidth = 2.0, color = 'black')
     plt.plot(x, y, color = "red", linewidth =3, label ="High")
     plt.plot(x, y1, color = "blue", linewidth =3, label ="Low")
     plt.legend(fontsize=12)
-    plt.savefig(f'{path1}temps_{qwe}')
-    
+    plt.savefig(f'{path1}tt_{qwe}')  
 
