8aaf52d1a414bcc4f305f0284b2ca27dd550232d
jimhnws <64051735+jimhnws@users.noreply.github.com>
2023-09-09 00:48:42 -0400

    Add files via upload

--- trWeatherTest.py
+++ trWeatherTest.py
@@ -4,36 +4,6 @@
 # In[ ]:
 
 
-'''
-import pandas as pd
-import dataFile
-import getNameNumbers
-import sqlalchemy
-import mysql.connector
-import sqlite3
-import NoDaysInMonth
-
-# creating connection
-conn = mysql.connector.connect(
-  host="3.135.162.69",
-  user="chuckwx",
-  password="jfr716!!00"
-)
-
-mycursor = conn.cursor()
-mycursor.execute("USE trweather;")
-pop1 = ("select @startTime := timeGroup from trw order by timeGroup DESC LIMIT 1;")
-pop2 = ("SELECT @endTime := DATE_SUB(@startTime, INTERVAL 30 Day);")
-pop3 = ("SELECT * from trw WHERE timeGroup BETWEEN @endTime AND @startTime into OUTFILE '/tmp/trw1.csv';")
-popSelect = pop1 + pop2 + pop3
-
-mycursor.execute(popSelect)
-'''
-
-
-# In[ ]:
-
-
 import pandas as pd
 import os
 
@@ -47,7 +17,7 @@ df = pd.read_csv(dataFile, index_col = False, sep = '\t', names = ['index', 'Rai
 newDate = df['Month'] + df['Day']
 df = df.drop(columns = ['index','HiTemp', 'LowTemp', 'timeGroup'], axis = 1)
 
-df.to_html(path2 + 'testRR' + '.html', index = False, justify = 'center')
+df.to_html(path2 + 'Rain30' + '.html', index = False, justify = 'center')
 
 
 # In[1]:
@@ -60,11 +30,13 @@ import matplotlib.dates as mdates
 import numpy as np
 import seaborn as sns
 
-#t = df['Rain']
-#y = t.to_numpy()
-#date = df['Day']
-#x = date.to_numpy()
-#print(x)
+path = '/tmp/'
+path1 = '/home/ec2-user/'
+path2 = '/var/www/html/000/'
+fileName = 'trw1.csv'
+dataFile = f'{path}{fileName}'
+
+df = pd.read_csv(dataFile, index_col = False, sep = '\t', names = ['index', 'Rain', 'HiTemp', 'LowTemp', 'Year', 'Month', 'Day', 'timeGroup'])
 
 year = (df['Year']).astype(str)
 month = (df['Month']).astype(str)
@@ -73,8 +45,8 @@ day = (df['Day']).astype(str)
 df['newDate'] = pd.to_datetime(df[['Year', 'Month', 'Day']])
 df['new_date'] = df['newDate'].dt.date
 df['new_date'].astype('str')
-df['new_date'] = df['new_date'].apply(str).str.replace('2023-', '')
-
+df['new_date'] = df['new_date'].apply(str).str.replace('2023-', '');print(df['new_date'])
+#df['new_date'] = df['new_date'].apply(str).str.replace('00:00:00', '')
 
 plt.figure(figsize= (10,6))
 plt.yticks(fontsize = 12)
@@ -85,7 +57,7 @@ plt.xlabel('Date',fontsize = 12, fontweight = 'bold')
 plt.ylabel('Rainfall (inches)', fontsize = 12, fontweight = 'bold')
 plt.grid(True)
 plt.autoscale(enable = True, axis = 'both', tight = True)
-plt.savefig(f'{path2}testIce')  
+plt.savefig(f'{path2}Rain30.png')  
 
 
 # In[ ]:
