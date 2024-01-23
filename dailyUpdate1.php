93e63adb7d028b811618bbc6ca45ebbd46d40f4b
jimhnws <64051735+jimhnws@users.noreply.github.com>
2024-01-22 23:40:35 -0500

    Add files via upload

--- dailyUpdate1.php
+++ dailyUpdate1.php
@@ -1,22 +1,24 @@
 <!DOCTYPE html>
-<html>
-    <head>
-    <meta charset="utf-8" />  
-    <meta name="viewport" content="width=device-width, initial-scale=1" />  
-    <link rel="stylesheet" media="screen and (min-width: 900px)" href="stylesQuery.css" />
-    <link rel="stylesheet" media="screen and (max-width: 600px) " href="stylesQueryMobile.css" />
+
+<head>
+    <html lang="en">
+    <link rel="stylesheet" media="screen and (min-width: 900px)" href="valTest.css" />
+    <link rel="stylesheet" media="screen and (max-width: 600px) " href="valTestMobile.css" />
     <link rel="stylesheet" href="normalize.css">
+    <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Get Monthly Data from the Database</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Get Daily Data from the Database</title>
 </head>
 
 <body>
 
-    <p>Monthly Almanac</p>
-    <div class = "query_boxMonth">
-        <form action=" monthlyUpdate1.php" method="post">
-        <label>Year</label>
-            <select id="Year" name="Year" style="font-size: 4vw;">
+    <p>Daily Almanac</p>
+    <div class="testBox">
+
+        <form action="dailyUpdate1.php" method="post">
+            <label>Year</label>
+            <select id="Year" name="Year" style="font-size: 3vw;">
                 <option value="1978">1978</option>
                 <option value="1979">1979</option>
                 <option value="1980">1980</option>
@@ -30,10 +32,7 @@
                 <option value="1990">1990</option>
                 <option value="1991">1991</option>
                 <option value="1992">1992</option>
-                <option value="1993">1993</option>
                 <option value="1995">1995</option>
-                <option value="1996">1996</option>
-                <option value="1997">1997</option>
                 <option value="1998">1998</option>
                 <option value="1999">1999</option>
                 <option value="2000">2000</option>
@@ -48,7 +47,6 @@
                 <option value="2009">2009</option>
                 <option value="2010">2010</option>
                 <option value="2011">2011</option>
-                <option value="2012">2012</option>
                 <option value="2013">2013</option>
                 <option value="2014">2014</option>
                 <option value="2015">2015</option>
@@ -59,11 +57,11 @@
                 <option value="2020">2020</option>
                 <option value="2021">2021</option>
                 <option value="2022">2022</option>
-                <option value="2023">2023</option>
+                <option value="2023">2023</option>  
                 <option value="2024">2024</option>
-            </select><br>
+            </select>
             <label>Month</label>
-            <select id="Month" name="Month" style="font-size: 4vw;">
+            <select id="Month" name="Month" style="font-size: 3vww;">
                 <option value="1">Jan</option>
                 <option value="2">Feb</option>
                 <option value="3">Mar</option>
@@ -76,85 +74,110 @@
                 <option value="10">Oct</option>
                 <option value="11">Nov</option>
                 <option value="12">Dec</option>
+            </select>
+            <label>Day</label>
+            <select id="Date" name="Date" style="font-size: 3vw;">
+                <option value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+                <option value="4">4</option>
+                <option value="5">5</option>
+                <option value="6">6</option>
+                <option value="7">7</option>
+                <option value="8">8</option>
+                <option value="9">9</option>
+                <option value="10">10</option>
+                <option value="11">11</option>
+                <option value="12">12</option>
+                <option value="13">13</option>
+                <option value="14">14</option>
+                <option value="15">15</option>
+                <option value="16">16</option>
+                <option value="17">17</option>
+                <option value="18">18</option>
+                <option value="19">19</option>
+                <option value="20">20</option>
+                <option value="21">21</option>
+                <option value="22">22</option>
+                <option value="23">23</option>
+                <option value="24">24</option>
+                <option value="25">25</option>
+                <option value="26">26</option>
+                <option value="27">27</option>
+                <option value="28">28</option>
+                <option value="29">29</option>
+                <option value="30">30</option>
+                <option value="31">31</option>
             </select><br>
             <input type="submit" value="Submit">
-        </form>    
-    </div>    
-
+        </form>
+    </div>
 <?php
 
-if($_POST['Month'] and $_POST['Year']){
-    $month = $_POST['Month'];
-    $year = $_POST['Year'];
-}           
+// Check if the form is submitted 
+    
+if($_POST['Month'] and $_POST['Year'] and $_POST['Date']){
+        $year = $_POST['Year'];
+        $month = $_POST['Month'];
+        $day = $_POST['Date'];
+            
+}   
 
 $servername = "3.135.162.69";
 $username = "chuckwx";
 $password = "jfr716!!00";
 $dbname = "trweather";
 
+// Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
-
+// Check connection
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "SELECT * from trw WHERE Month = $month AND Year = $year";
+$sql = "SELECT * from trw WHERE Year = $year AND Month = $month AND Day = $day";
 $result = $conn->query($sql);
 
 $chuck = $result->fetch_all(MYSQLI_ASSOC);
 $chuckwx = json_encode($chuck);
 //echo '<prev>'; print_r($chuckwx); echo '</prev>';
 
-$file = fopen("/var/www/html/000/monthly.txt", "w") or die("Unable to open file");
+$file = fopen("/var/www/html/000/daily.txt", "w") or die("Unable to open file");
 
 fwrite($file, $chuckwx);
 fclose($file);
 
 $conn->close();
 
-$output = shell_exec('/var/www/html/000/testBlast.sh 2>&1');
+$output = shell_exec('/var/www/html/000/dailyProcess.sh 2>&1');
 //var_dump($output);
-
-sleep(2);
+//header("Location: http://3.135.162.69/dailyTest.html");
 
 ?>
 
 <div class="hold">
-
-    <iframe src="http://3.135.162.69/try1.html" name="targetframe" allowTransparency="true" scrolling="no" frameborder="0" width="800" height="750">
+    <iframe src="http://3.135.162.69/dailyTest.html" name="targetframe" allowTransparency="true" scrolling="no" frameborder="0">
     </iframe>
 </div>      
 
-<div class="hold1">
-    <iframe src="http://3.135.162.69/try2.html" name="targetframe" allowTransparency="true" scrolling="no" frameborder="0" width="400" height="200">
-    </iframe>
-</div>
-
-
 <!--
-<div id="display"></div>
-<script>
-  function load_anotherpage() {
-    document.getElementById("display").innerHTML =
-      '<embed type="text/html" src="http://3.135.162.69/try1.html" width="440" height="800">';
-  }  
+<div class="hold">
+    <div id="display1"></div>
+    <script>
+        function load_anotherpage() {
+          document.getElementById("display1").innerHTML =
+           '<embed type="text/html" src="http://3.135.162.69/dailyTest.html" width="500" height="500">';
+    }
 
-  load_anotherpage();
-</script>  
--->
+         load_anotherpage();
+    </script>  
+</div>      
 
-<div class="image1">
-    <iframe src="http://3.135.162.69/allInOne.png" name="targetframe" allowTransparency="true" scrolling="no" frameborder="0" width="1000" height="800">
-    </iframe>
-</div>
-   
-<!--
-<div id="display1"></div>
+<div id="display1" style="position: absolute;top: 0;left: 15%;"></div>
 <script>
   function load_anotherpage() {
     document.getElementById("display1").innerHTML =
-      '<embed type="text/html" src="http://3.135.162.69/try2.html" width="425" height="200">';
+      '<embed type="text/html" src="http://3.135.162.69/dailyTest.html" width="1500" height="1500">';
   }
 
   load_anotherpage();
